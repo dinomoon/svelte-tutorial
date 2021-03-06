e0ebe2d184ecfe1c4a4466be6a535097f06d9ff2
MoonK <ansrud45@gmail.com>
2021-03-06 11:52:44 +0900

    Header & Footer components

---
+++ src/components/Footer.svelte
@@ -0,0 +1,22 @@
+<script>
+  let year = new Date().getFullYear();
+</script>
+
+<footer>
+  <div class="copyright">Copyright {year} Poll Ninja</div>
+</footer>
+
+<style>
+  footer {
+    padding: 40px;
+    text-align: center;
+  }
+
+  .copyright {
+    color: #aaa;
+    font-size: 14px;
+    display: inline-block;
+    padding: 20px;
+    border-top: 1px solid #ddd;
+  }
+</style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,21 @@
+<header>
+  <h1>
+    <img src="https://raw.githubusercontent.com/iamshaunjp/svelte-tutorial/e3767222c8dfeb74a0f2640e25e5858795a68190/ninjapolls/public/img/poll_ninja_logo.svg" alt="Poll Ninha Logo">
+  </h1>
+</header>
+
+<style>
+  header {
+    background: #f7f7f7;
+    padding: 20px;
+  }
+
+  h1 {
+    margin: 0;
+    text-align: center;
+  }
+
+  img {
+    max-width: 180px;
+  }
+</style>
