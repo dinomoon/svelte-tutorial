bb8abf664eac620531f56080f70e58e779bfbc2a
MoonK <ansrud45@gmail.com>
2021-03-06 16:17:43 +0900

    Casting Votes

--- src/components/PollDetail.svelte
+++ src/components/PollDetail.svelte
@@ -1,20 +1,28 @@
 <script>
+  import { createEventDispatcher } from 'svelte';
   import Card from '../shared/Card.svelte';
+  
   export let poll;
+  let dispatch = createEventDispatcher()
 
   // reactive values
   $: totalVotes = poll.votesA + poll.votesB;
+
+  // handling votes
+  const handleVote = (option, id) => {
+    dispatch('vote', {option, id});
+  }
 </script>
 
 <Card>
   <div class="poll">
     <h2>{poll.question}</h2>
     <p>Total votes: {totalVotes}</p>
-    <div class="answer">
+    <div class="answer" on:click={() => handleVote('a', poll.id)}>
       <div class="percent percent-a"></div>
       <span>{poll.answerA} ({poll.votesA})</span>
     </div>
-    <div class="answer">
+    <div class="answer" on:click={() => handleVote('b', poll.id)}>
       <div class="percent percent-b"></div>
       <span>{poll.answerB} ({poll.votesB})</span>
     </div>
